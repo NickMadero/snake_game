0e9a52ee6a67904f4fb48f813c456f86f36ab379
NickMadero <madero22@students.rowan.edu>
2022-06-05 11:53:04 -0400

    A finished draft of snake game needs polishing.

--- snake.py
+++ snake.py
@@ -7,9 +7,9 @@ delay = .1
 score = 0
 high_score = 0
 
-window = turtle.screen()
+window = turtle.Screen()
 window.title("snake game")
-window.background("black")
+window.bgcolor("black")
 window.setup(width=600, height=600)
 window.tracer(0)
 
@@ -99,3 +99,60 @@ while True:
 
         score = 0
         delay = .1
+
+        pen.clear()
+        pen.write("score: {} Highscore: {} ".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    if head.distance(food) < 20:
+
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("triangle")
+        new_segment.color("blue")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        delay -= .001
+        score += 10
+
+        if score > high_score:
+            high_score = score
+
+        pen.clear()
+        pen.write("score {} highscore: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    move()
+
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            segments.clear()
+
+            score = 0
+            delay = .1
+
+            pen.clear()
+            pen.write("score {} highscore: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
+window.mainloop()
